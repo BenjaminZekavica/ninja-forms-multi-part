c92d873edd48764f2f83547fe8e4f3daadd0eef4
Kevin Stover <kstover@gmail.com>
2013-07-24 01:28:06 +0100

    The "_page_divider" field should now register even if Multi-Part isn't active for the particular form.

--- includes/fields/page-divider.php
+++ includes/fields/page-divider.php
@@ -1,63 +1,42 @@
 <?php
-add_action( 'init', 'ninja_forms_register_field_page_divider' );
+add_action( 'init', 'ninja_forms_register_field_page_divider', 9 );
 
 function ninja_forms_register_field_page_divider( $form_id = '' ){
 	global $ninja_forms_processing;
 
-	if( isset( $_REQUEST['form_id'] ) ){
-		$form_id = $_REQUEST['form_id'];
-	}
-	
-
-	if( is_object( $ninja_forms_processing ) ){
-		$form_id = $ninja_forms_processing->get_form_ID();
-	}
-
-	if( $form_id != '' AND $form_id != 'new' AND $form_id != 'all' OR ( is_admin() AND isset( $_REQUEST['page'] ) AND $_REQUEST['page'] == 'ninja-forms-subs' )  ){
-		$form_row = ninja_forms_get_form_by_id( $form_id );
-		$form_data = $form_row['data'];
-		if( isset( $form_data['multi_part'] ) ){
-			$multi_part = $form_data['multi_part'];
-		}else{
-			$multi_part = 0;
-		}
-		
-		if( $multi_part == 1 OR ( is_admin() AND isset( $_REQUEST['page'] ) AND $_REQUEST['page'] == 'ninja-forms-subs' ) ){
-			$args = array(
-				'name' => 'Page Divider',
-				'sidebar' => '',
-				'edit_function' => 'ninja_forms_field_page_divider_edit',
-				'display_function' => '',
-				'save_function' => '',
-				'group' => '',
-				'edit_label' => false,
-				'edit_label_pos' => false,
-				'edit_req' => false,
-				'edit_custom_class' => false,
-				'edit_help' => false,
-				'edit_meta' => false,
-				'edit_conditional' => true,
-				'process_field' => false,
-				'use_li' => false,
-				'conditional' => array(
-					'action' => array(
-						'show' => array(
-							'name' => 'Show This',
-							'js_function' => 'ninja_forms_show_mp_page',
-							'output' => 'show',
-						),				
-						'hide' => array(
-							'name' => 'Hide This',
-							'js_function' => 'ninja_forms_hide_mp_page',
-							'output' => 'hide',
-						),			
-					),
-				),
-			);
-			if( function_exists( 'ninja_forms_register_field' ) ){
-				ninja_forms_register_field( '_page_divider', $args );
-			}
-		}
+	$args = array(
+		'name' => 'Page Divider',
+		'sidebar' => '',
+		'edit_function' => 'ninja_forms_field_page_divider_edit',
+		'display_function' => '',
+		'save_function' => '',
+		'group' => '',
+		'edit_label' => false,
+		'edit_label_pos' => false,
+		'edit_req' => false,
+		'edit_custom_class' => false,
+		'edit_help' => false,
+		'edit_meta' => false,
+		'edit_conditional' => true,
+		'process_field' => false,
+		'use_li' => false,
+		'conditional' => array(
+			'action' => array(
+				'show' => array(
+					'name' => 'Show This',
+					'js_function' => 'ninja_forms_show_mp_page',
+					'output' => 'show',
+				),				
+				'hide' => array(
+					'name' => 'Hide This',
+					'js_function' => 'ninja_forms_hide_mp_page',
+					'output' => 'hide',
+				),			
+			),
+		),
+	);
+	if( function_exists( 'ninja_forms_register_field' ) ){
+		ninja_forms_register_field( '_page_divider', $args );
 	}
 }
 

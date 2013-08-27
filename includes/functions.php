609e63ab221f3a24efeb16589fbe196ff198a543
Kevin Stover <kstover@gmail.com>
2013-08-27 19:30:34 +0100

    Added a function ninja_forms_mp_get_page_by_field_id that returns the page number a given field is on.

--- includes/functions.php
+++ includes/functions.php
@@ -41,3 +41,23 @@ function ninja_forms_mp_get_page_by_divider( $form_id, $field_id ){
 
 	return $page;
 }
+
+function ninja_forms_mp_get_page_by_field_id( $field_id ) {
+	$field = ninja_forms_get_field_by_id( $field_id );
+	$form_id = $field['form_id'];
+	$pages = ninja_forms_mp_get_pages( $form_id );
+	// Loop through our pages until we find this field.
+	if ( is_array ( $pages ) ) {
+		foreach ( $pages as $num => $page ) {
+			if ( is_array( $page ) ) {
+				foreach ( $page as $field ) {
+					if ( $field['id'] == $field_id ) {
+						$page = $num;
+						break 2;
+					}
+				}
+			}
+		}
+	}
+	return $page;
+}

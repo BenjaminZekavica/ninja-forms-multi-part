ae05ed8cdade6b51351feccb3b0badd16c294918
Kevin Stover <kstover@gmail.com>
2013-09-23 11:25:57 +0100

    Changing the license registration to the new method available in Ninja Forms 2.2.47.

--- multi-part.php
+++ multi-part.php
@@ -3,44 +3,22 @@
 Plugin Name: Ninja Forms - Multi-Part Forms
 Plugin URI: http://ninjaforms.com/downloads/multi-part-forms/
 Description: Multi-Part Forms add-on for Ninja Forms.
-Version: 1.0.12
+Version: 1.0.13
 Author: The WP Ninjas
 Author URI: http://ninjaforms.com
 */
 
 define("NINJA_FORMS_MP_DIR", WP_PLUGIN_DIR."/".basename( dirname( __FILE__ ) ) );
 define("NINJA_FORMS_MP_URL", plugins_url()."/".basename( dirname( __FILE__ ) ) );
-define("NINJA_FORMS_MP_VERSION", "1.0.12");
+define("NINJA_FORMS_MP_VERSION", "1.0.13");
 
-// this is the URL our updater / license checker pings. This should be the URL of the site with EDD installed
-define( 'NINJA_FORMS_MP_EDD_SL_STORE_URL', 'http://ninjaforms.com' ); // IMPORTANT: change the name of this constant to something unique to prevent conflicts with other plugins using this system
-
-// the name of your product. This is the title of your product in EDD and should match the download title in EDD exactly
-define( 'NINJA_FORMS_MP_EDD_SL_ITEM_NAME', 'Multi-Part Forms' ); // IMPORTANT: change the name of this constant to something unique to prevent conflicts with other plugins using this system
-
-//Require EDD autoupdate file
-if( !class_exists( 'EDD_SL_Plugin_Updater' ) ) {
-	// load our custom updater if it doesn't already exist
-	require_once( NINJA_FORMS_MP_DIR.'/includes/EDD_SL_Plugin_Updater.php' );
+function ninja_forms_mp_setup_license() {
+	if ( class_exists( 'NF_Extension_Updater' ) ) {
+		$NF_Extension_Updater = new NF_Extension_Updater( 'Multi-Part Forms', NINJA_FORMS_MP_VERSION, 'WP Ninjas', __FILE__, 'mp' );
+	}
 }
 
-$plugin_settings = get_option( 'ninja_forms_settings' );
-
-// retrieve our license key from the DB
-if( isset( $plugin_settings['mp_license'] ) ){
-	$mp_license = $plugin_settings['mp_license'];
-}else{
-	$mp_license = '';
-}
-
-// setup the updater
-$edd_updater = new EDD_SL_Plugin_Updater( NINJA_FORMS_MP_EDD_SL_STORE_URL, __FILE__, array(
-		'version' 	=> NINJA_FORMS_MP_VERSION, 		// current version number
-		'license' 	=> $mp_license, 	// license key (used get_option above to retrieve from DB)
-		'item_name'     => NINJA_FORMS_MP_EDD_SL_ITEM_NAME, 	// name of this plugin
-		'author' 	=> 'WP Ninjas'  // author of this plugin
-	)
-);
+add_action( 'admin_init', 'ninja_forms_mp_setup_license' );
 
 require_once(NINJA_FORMS_MP_DIR."/includes/admin/open-div.php");
 require_once(NINJA_FORMS_MP_DIR."/includes/admin/close-div.php");
@@ -48,7 +26,6 @@ require_once(NINJA_FORMS_MP_DIR."/includes/admin/edit-field-ul.php");
 require_once(NINJA_FORMS_MP_DIR."/includes/admin/scripts.php");
 require_once(NINJA_FORMS_MP_DIR."/includes/admin/ajax.php");
 require_once(NINJA_FORMS_MP_DIR."/includes/admin/form-settings-metabox.php");
-require_once(NINJA_FORMS_MP_DIR."/includes/admin/license-option.php");
 
 require_once(NINJA_FORMS_MP_DIR."/includes/display/nav.php");
 require_once(NINJA_FORMS_MP_DIR."/includes/display/filter-fields.php");

70a46ac9804c4222f5731eaea8b82a4c85c83f96
Kevin Stover <kstover@gmail.com>
2016-08-17 15:39:15 -0400

    Progress bar should now fill to 100% on submission.

--- assets/js/front-end/views/header.js
+++ assets/js/front-end/views/header.js
@@ -1,10 +1,13 @@
 define( [], function() {
 	var view = Marionette.ItemView.extend( {
 		template: "#nf-tmpl-mp-header",
- 		
+ 		fullProgressBar: false,
+
  		initialize: function( options ) {
 			this.listenTo( this.collection, 'change:part', this.reRender );
 			this.listenTo( this.collection, 'change:errors', this.reRender );
+
+			this.listenTo( nfRadio.channel( 'forms' ), 'before:submit', this.fillProgressBar );
 		},
 
 		reRender: function() {
@@ -24,7 +27,7 @@ define( [], function() {
 				renderProgressBar: function() {
 					var template = _.template( jQuery( '#nf-tmpl-mp-progress-bar' ).html() );
 					var currentIndex = that.collection.getVisibleParts().indexOf( that.model );
-					var percent = currentIndex / that.collection.getVisibleParts().length * 100;
+					var percent = ( that.fullProgressBar ) ? 100 : currentIndex / that.collection.getVisibleParts().length * 100;
 					return template( { percent: percent } );
 				}
 			}
@@ -37,6 +40,12 @@ define( [], function() {
 		clickBreadcrumb: function( e ) {
 			e.preventDefault();
 			this.collection.setElement( this.collection.getVisibleParts()[ jQuery( e.target ).data( 'index' ) ] );
+		},
+
+		fillProgressBar: function( formModel ) {
+			this.fullProgressBar = true;
+			this.render();
+			this.fullProgressBar = false;
 		}
 	} );
 

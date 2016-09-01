885474306f924142aff3cecd829d853e85a40495
Kyle B. Johnson <kyle@wpninjas.com>
2016-09-01 16:42:33 -0400

    update template tags.

--- includes/Templates/builder.html.php
+++ includes/Templates/builder.html.php
@@ -1,22 +1,24 @@
-<?php if( false ): ?>
+<?php $update_templates = version_compare( WPN_Helper::get_plugin_version( 'ninja-forms/ninja-forms.php' ), '3', '>' ); ?>
+<?php if( $update_templates ): ?>
 	<script id="nf-tmpl-mp-gutter-left" type="text/template">
-		<# if ( hasPrevious() ) { #>
+		<# if ( data.hasPrevious() ) { #>
 		<i class="fa fa-chevron-circle-left" aria-hidden="true"></i>
 		<# } #>
 	</script>
 <?php else: ?>
 	<script id="nf-tmpl-mp-gutter-left" type="text/template">
+		There
 		<% if ( hasPrevious() ) { %>
 		<i class="fa fa-chevron-circle-left" aria-hidden="true"></i>
 		<% } %>
 	</script>
 <?php endif; ?>
 
-<?php if( false ): ?>
+<?php if( $update_templates ): ?>
 	<script id="nf-tmpl-mp-gutter-right" type="text/template">
-		<# if ( hasNext() ) { #>
+		<# if ( data.hasNext() ) { #>
 		<i class="fa fa-chevron-circle-right next" aria-hidden="true"></i>
-		<# } else if ( hasContent() ) { #>
+		<# } else if ( data.hasContent() ) { #>
 		<i class="fa fa-plus-circle new" aria-hidden="true"></i>
 		<#} #>
 	</script>
@@ -30,7 +32,7 @@
 	</script>
 <?php endif; ?>
 
-<?php if( false ): ?>
+<?php if( $update_templates ): ?>
 	<script id="nf-tmpl-mp-layout" type="text/template">
 		<div id="nf-mp-main-content"></div>
 		<div id="nf-mp-drawer"></div>
@@ -43,7 +45,7 @@
 <?php endif; ?>
 
 
-<?php if( false ): ?>
+<?php if( $update_templates ): ?>
 	<script id="nf-tmpl-mp-main-content-fields-empty" type="text/template">
 		<div class="nf-fields-empty">
 			<h3><?php _e( 'Add form fields', 'ninja-forms' ); ?></h3>
@@ -59,9 +61,9 @@
 	</script>
 <?php endif; ?>
 
-<?php if( false ): ?>
+<?php if( $update_templates ): ?>
 	<script id="nf-tmpl-mp-drawer-item" type="text/template">
-		{{{ title }}}
+		{{{ data.title }}}
 	</script>
 <?php else: ?>
 	<script id="nf-tmpl-mp-drawer-item" type="text/template">
@@ -69,9 +71,9 @@
 	</script>
 <?php endif; ?>
 
-<?php if( false ): ?>
+<?php if( $update_templates ): ?>
 	<script id="nf-tmpl-mp-form-title" type="text/template">
-		<h2>{{{ renderFormTitle() }}} - <i class="fa fa-cog" aria-hidden="true"></i> {{{ renderPartTitle() }}}</h2>
+		<h2>{{{ data.renderFormTitle() }}} - <i class="fa fa-cog" aria-hidden="true"></i> {{{ data.renderPartTitle() }}}</h2>
 	</script>
 <?php else: ?>
 	<script id="nf-tmpl-mp-form-title" type="text/template">
@@ -80,7 +82,7 @@
 <?php endif; ?>
 
 
-<?php if( false ): ?>
+<?php if( $update_templates ): ?>
 	<script id="nf-tmpl-mp-drawer-layout" type="text/template">
 		<div class="nf-mp-drawer-scroll-previous nf-mp-drawer-scroll" style="">
 			<i class="fa fa-chevron-left" aria-hidden="true"></i>
--- includes/Templates/frontend.html.php
+++ includes/Templates/frontend.html.php
@@ -1,4 +1,5 @@
-<?php if( false ): ?>
+<?php $update_templates = version_compare( WPN_Helper::get_plugin_version( 'ninja-forms/ninja-forms.php' ), '3', '>' ); ?>
+<?php if( $update_templates ): ?>
 	<script id="nf-tmpl-mp-form-content" type="text/template">
 		<div class="nf-mp-header"></div>
 		<div class="nf-mp-body"></div>
@@ -12,11 +13,11 @@
 	</script>
 <?php endif; ?>
 
-<?php if( false ): ?>
+<?php if( $update_templates ): ?>
 	<script id="nf-tmpl-mp-header" type="text/template">
-		{{{ renderBreadcrumbs() }}}
+		{{{ data.renderBreadcrumbs() }}}
 		<h3>
-			<%= title %>
+			{{{ data.title }}}
 		</h3>
 	</script>
 <?php else: ?>
@@ -28,9 +29,9 @@
 	</script>
 <?php endif; ?>
 
-<?php if( false ): ?>
+<?php if( $update_templates ): ?>
 	<script id="nf-tmpl-mp-footer" type="text/template">
-		{{{ renderNextPrevious() }}}
+		{{{ data.renderNextPrevious() }}}
 	</script>
 <?php else: ?>
 	<script id="nf-tmpl-mp-footer" type="text/template">
@@ -38,17 +39,17 @@
 	</script>
 <?php endif; ?>
 
-<?php if( false ): ?>
+<?php if( $update_templates ): ?>
 	<script id="nf-tmpl-mp-next-previous" type="text/template">
-		<# if ( ! showNext && ! showPrevious ) return #>
+		<# if ( ! data.showNext && ! data.showPrevious ) return #>
 			<ul>
-				<# if ( showPrevious ) { #>
+				<# if ( data.showPrevious ) { #>
 					<li>
 						<a href="#" class="nf-previous">PREVIOUS</a>
 					</li>
 					<# } #>
 
-						<# if ( showNext ) { #>
+						<# if ( data.showNext ) { #>
 							<li>
 								<a href="#" class="nf-next">NEXT</a>
 							</li>
@@ -74,11 +75,11 @@
 	</script>
 <?php endif; ?>
 
-<?php if( false ): ?>
+<?php if( $update_templates ): ?>
 	<script id="nf-tmpl-mp-breadcrumbs" type="text/template">
 		<ul>
-			<# _.each( parts, function( part, index ) { #>
-				<li class="{{{ ( currentIndex == index ) ? 'active' : '' }}} {{{ ( part.errors ) ? 'errors' : '' }}}">
+			<# _.each( data.parts, function( part, index ) { #>
+				<li class="{{{ ( data.currentIndex == index ) ? 'active' : '' }}} {{{ ( part.errors ) ? 'errors' : '' }}}">
 					<a href="#" class="nf-breadcrumb" data-index="{{{ index }}}">{{{ part.title }}}</a>
 					{{{ ( part.errors ) ? '<-- ERRORS' : '' }}}
 				</li>

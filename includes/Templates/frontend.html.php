285830203bf170b93c0c71286c8b338fc2c61a6d
Kyle B. Johnson <kyle@wpninjas.com>
2016-09-01 17:34:46 -0400

    re-update frontend template tags.

--- includes/Templates/frontend.html.php
+++ includes/Templates/frontend.html.php
@@ -1,27 +1,22 @@
 <?php $update_templates = version_compare( WPN_Helper::get_plugin_version( 'ninja-forms/ninja-forms.php' ), '3', '>' ); ?>
-<?php if( $update_templates ): ?>
-	<script id="nf-tmpl-mp-form-content" type="text/template">
-		<div class="nf-mp-header"></div>
-		<div class="nf-mp-body"></div>
-		<div class="nf-mp-footer"></div>
-	</script>
-<?php else: ?>
-	<script id="nf-tmpl-mp-form-content" type="text/template">
-		<div class="nf-mp-header"></div>
-		<div class="nf-mp-body"></div>
-		<div class="nf-mp-footer"></div>
-	</script>
-<?php endif; ?>
+
+<script id="nf-tmpl-mp-form-content" type="text/template">
+	<div class="nf-mp-header"></div>
+	<div class="nf-mp-body"></div>
+	<div class="nf-mp-footer"></div>
+</script>
 
 <?php if( $update_templates ): ?>
 	<script id="nf-tmpl-mp-header" type="text/template">
-		{{{ data.renderBreadcrumbs() }}}
+		{{{ data.renderProgressBar() }}}
+		{{ data.renderBreadcrumbs() }}}
 		<h3>
 			{{{ data.title }}}
 		</h3>
 	</script>
 <?php else: ?>
 	<script id="nf-tmpl-mp-header" type="text/template">
+		<%= renderProgressBar() %>
 		<%= renderBreadcrumbs() %>
 		<h3>
 			<%= title %>
@@ -41,34 +36,32 @@
 
 <?php if( $update_templates ): ?>
 	<script id="nf-tmpl-mp-next-previous" type="text/template">
-		<# if ( ! data.showNext && ! data.showPrevious ) return #>
-			<ul>
-				<# if ( data.showPrevious ) { #>
-					<li>
-						<a href="#" class="nf-previous">PREVIOUS</a>
-					</li>
-					<# } #>
+		<ul class="nf-next-previous">
+			<# if ( data.showPrevious ) { #>
+			<li class="nf-previous-item">
+				<input type="button" class="nf-previous" value="Previous" />
+			</li>
+			<# } #>
 
-						<# if ( data.showNext ) { #>
-							<li>
-								<a href="#" class="nf-next">NEXT</a>
-							</li>
-							<# } #>
-			</ul>
+			<# if ( data.showNext ) { #>
+			<li class="nf-next-item">
+				<input type="button" class="nf-next" value="Next" />
+			</li>
+			<# } #>
+		</ul>
 	</script>
 <?php else: ?>
 	<script id="nf-tmpl-mp-next-previous" type="text/template">
-		<% if ( ! showNext && ! showPrevious ) return %>
-		<ul>
+		<ul class="nf-next-previous">
 			<% if ( showPrevious ) { %>
-			<li>
-				<a href="#" class="nf-previous">PREVIOUS</a>
+			<li class="nf-previous-item">
+				<input type="button" class="nf-previous" value="Previous" />
 			</li>
 			<% } %>
 
 			<% if ( showNext ) { %>
-			<li>
-				<a href="#" class="nf-next">NEXT</a>
+			<li class="nf-next-item">
+				<input type="button" class="nf-next" value="Next" />
 			</li>
 			<% } %>
 		</ul>
@@ -76,26 +69,33 @@
 <?php endif; ?>
 
 <?php if( $update_templates ): ?>
-	<script id="nf-tmpl-mp-breadcrumbs" type="text/template">
-		<ul>
-			<# _.each( data.parts, function( part, index ) { #>
-				<li class="{{{ ( data.currentIndex == index ) ? 'active' : '' }}} {{{ ( part.errors ) ? 'errors' : '' }}}">
-					<a href="#" class="nf-breadcrumb" data-index="{{{ index }}}">{{{ part.title }}}</a>
-					{{{ ( part.errors ) ? '<-- ERRORS' : '' }}}
-				</li>
-				<# } ); #>
-		</ul>
-	</script>
+<script id="nf-tmpl-mp-breadcrumbs" type="text/template">
+	<ul class="nf-breadcrumbs">
+		<# _.each( data.parts, function( part, index ) { #>
+		<li class="{{{ ( data.currentIndex == index ) ? 'active' : '' }}} {{{ ( part.errors ) ? 'errors' : '' }}}">
+			<a href="#" class="nf-breadcrumb" data-index="{{{ index }}}">{{{ ( part.errors ) ? '' : '' }}} {{{ part.title }}}</a>
+		</li>
+		<# } ); #>
+	</ul>
+</script>
 <?php else: ?>
-	<script id="nf-tmpl-mp-breadcrumbs" type="text/template">
-		<ul>
-			<% _.each( parts, function( part, index ) { %>
-			<li class="<%= ( currentIndex == index ) ? 'active' : '' %> <%= ( part.errors ) ? 'errors' : '' %>">
-				<a href="#" class="nf-breadcrumb" data-index="<%= index %>"><%= part.title %></a>
-				<%= ( part.errors ) ? '<-- ERRORS' : '' %>
-			</li>
-			<% } ); %>
-		</ul>
-	</script>
+<script id="nf-tmpl-mp-breadcrumbs" type="text/template">
+	<ul class="nf-breadcrumbs">
+		<% _.each( parts, function( part, index ) { %>
+		<li class="<%= ( currentIndex == index ) ? 'active' : '' %> <%= ( part.errors ) ? 'errors' : '' %>">
+			<a href="#" class="nf-breadcrumb" data-index="<%= index %>"><%= ( part.errors ) ? '' : '' %> <%= part.title %></a>
+		</li>
+		<% } ); %>
+	</ul>
+</script>
 <?php endif; ?>
 
+<?php if( $update_templates ): ?>
+<script id="nf-tmpl-mp-progress-bar" type="text/template">
+	<progress style="width:100%" value="{{{ data.percent }}}" max="100">{{{ data.percent }}} %</progress>
+</script>
+<?php else: ?>
+<script id="nf-tmpl-mp-progress-bar" type="text/template">
+	<progress style="width:100%" value="<%= percent %>" max="100"><%= percent %> %</progress>
+</script>
+<?php endif; ?>

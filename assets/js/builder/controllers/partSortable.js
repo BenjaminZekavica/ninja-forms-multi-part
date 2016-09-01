88f302211ac159e11d2b90f193f65c702d8fc3db
Kevin Stover <kstover@gmail.com>
2016-09-01 17:04:00 -0400

    You can now undo sorting parts.

--- assets/js/builder/controllers/partSortable.js
+++ assets/js/builder/controllers/partSortable.js
@@ -30,12 +30,45 @@ define(	[],	function () {
 		},
 
 		update: function( e, ui, collection, collectionView ) {
+			var partModel = collection.findWhere( { key: jQuery( ui.item ).prop( 'id' ) } );
+			/*
+			 * Store our current order.
+			 */
+			var oldOrder = {};
+			collection.each( function( partModel, index ) {
+				oldOrder[ partModel.get( 'key' ) ] = index;
+			} );
+
 			jQuery( ui.item ).css( 'opacity', '' );
+
 			var order = _.without( jQuery( collectionView.el ).sortable( 'toArray' ), '' );
-			_.each( order, function( cid, index ) {
-				collection.get( { cid: cid } ).set( 'order', index );
+			_.each( order, function( key, index ) {
+				collection.findWhere( { key: key } ).set( 'order', index );
 			}, this );
 			collection.sort();
+
+			/*
+			 * Register our part change to the change manager.
+			 */
+			//Set our 'clean' status to false so that we get a notice to publish changes
+			nfRadio.channel( 'app' ).request( 'update:setting', 'clean', false );
+			// Update our preview
+			nfRadio.channel( 'app' ).request( 'update:db' );
+
+			// Add our field addition to our change log.
+			var label = {
+				object: 'Part',
+				label: partModel.get( 'title' ),
+				change: 'Sorted',
+				dashicon: 'sort'
+			};
+
+			var data = {
+				oldOrder: oldOrder,
+				collection: collection
+			};
+
+			var newChange = nfRadio.channel( 'changes' ).request( 'register:change', 'sortParts', partModel, null, label, data );
 		},
 
 	});

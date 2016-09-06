497de1467ed7c09f54899daf5642217b0f2aebd1
Kyle B. Johnson <kyle@wpninjas.com>
2016-09-05 22:23:28 -0400

    update tempalte check.

--- includes/Templates/frontend.html.php
+++ includes/Templates/frontend.html.php
@@ -1,4 +1,4 @@
-<?php $update_templates = version_compare( WPN_Helper::get_plugin_version( 'ninja-forms/ninja-forms.php' ), '3', '>' ); ?>
+<?php $update_templates = version_compare( get_option( 'ninja_forms_version', '0' ), '3', '>' ); ?>
 
 <script id="tmpl-nf-mp-form-content" type="text/template">
 	<div class="nf-mp-header"></div>

6ed1fe0093b84099f4427758ddad70ea0cf3a16d
Kevin Stover <kstover@gmail.com>
2016-09-07 13:24:38 -0400

    Adding a new part no longer opens a drawer automatically.

--- assets/js/builder/models/partCollection.js
+++ assets/js/builder/models/partCollection.js
@@ -22,68 +22,17 @@ define( [ 'models/partModel' ], function( PartModel ) {
 		},
 
 		afterAdd: function( model ) {
-			/*
-			 * If we only have one model and it's set to clean, open a drawer so that the user can edit its title.
-			 */
-			if ( this.at( 0 ).get( 'clean' ) && 2 == this.length ) {
-				/*
-				 * When the first part title drawer closes, continue our new part processing.
-				 */
-				this.listenToOnce( nfRadio.channel( 'drawer' ), 'closed', this.afterFirstDrawerClose, model );
-				this.openFirstDrawer( this.at( 0 ) );
-			} else {
-				this.openDrawer( model );
-				this.setElement( model );
-				this.maybeChangeBuilderClass( model );
-			}
+			this.setElement( model );
+			this.maybeChangeBuilderClass( model );
 		},
 
-		afterFirstDrawerClose: function() {
-			var newModel = this.at( _.max( this.pluck( 'order' ) ) );
-			var settingGroupCollection = nfRadio.channel( 'mp' ).request( 'get:settingGroupCollection' );
-			var settingModel = settingGroupCollection.get( 'primary' ).get( 'settings' ).findWhere( { name: 'title' } );
-			settingModel.set( 'label', settingModel.get( 'oldLabel' ) );
-			this.openDrawer( newModel );
-			this.setElement( newModel );
-			this.maybeChangeBuilderClass( newModel );
-		},
-
+		
 		maybeChangeBuilderClass: function( count, collection, options ) {
 			if ( true === count instanceof Backbone.Model ) {
 				count = this.length;
 			}
 
 			this.changeBuilderClass( 1 < count );
-		},
-		
-		openFirstDrawer: function( model ) {
-			var settingGroupCollection = nfRadio.channel( 'mp' ).request( 'get:settingGroupCollection' );
-			var settingModel = settingGroupCollection.get( 'primary' ).get( 'settings' ).findWhere( { name: 'title' } );
-			settingModel.set( 'oldLabel', settingModel.get( 'label' ) );
-			settingModel.set( 'label', 'Give your first part a title' );
-			nfRadio.channel( 'app' ).request( 'open:drawer', 'editSettings', { model: model, groupCollection: settingGroupCollection } );
-		
-			/*
-			 * When the drawer opens, focus the input box.
-			 */
-			nfRadio.channel( 'drawer' ).on( 'opened', this.focusTitle );
-		},
-
-		openDrawer: function( model ) {
-			var settingGroupCollection = nfRadio.channel( 'mp' ).request( 'get:settingGroupCollection' );
-			nfRadio.channel( 'app' ).request( 'open:drawer', 'editSettings', { model: model, groupCollection: settingGroupCollection } );
-		
-			/*
-			 * When the drawer opens, focus the input box.
-			 */
-			nfRadio.channel( 'drawer' ).on( 'opened', this.focusTitle );
-		},
-
-		focusTitle: function() {
-			var drawerEl = nfRadio.channel( 'app' ).request( 'get:drawerEl' );
-			jQuery( drawerEl ).find( '#title' ).select();
-			jQuery( drawerEl ).find( '#title' ).focus();
-			nfRadio.channel( 'drawer' ).off( 'opened', this.focusTitle );
 		},
 
 		changeBuilderClass: function( hasParts ) {

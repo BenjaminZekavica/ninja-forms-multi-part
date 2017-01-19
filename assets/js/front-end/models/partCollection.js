765ae4b769576b6491f21f336fc9b66164b62514
Kevin Stover <kstover@gmail.com>
2017-01-19 14:58:44 -0500

    Google Recaptcha fields should render properly if they are on parts beyond the first. Closes #77.

--- assets/js/front-end/models/partCollection.js
+++ assets/js/front-end/models/partCollection.js
@@ -29,6 +29,7 @@ define( [ 'models/partModel' ], function( PartModel ) {
 			this.currentElement = model;
 			if ( ! silent ) {
 				this.trigger( 'change:part', this );
+				console.log( 'trigger change part' );
 				nfRadio.channel( 'nfMP' ).trigger( 'change:part', this );
 			} 
 		},

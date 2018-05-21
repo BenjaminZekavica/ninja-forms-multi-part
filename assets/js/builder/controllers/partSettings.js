2b401b3ff04307405af210abdaec27bac0e358fd
Eric Windham <eric.windham@gmail.com>
2018-05-21 10:54:14 -0400

    changes made per request

--- assets/js/builder/controllers/partSettings.js
+++ assets/js/builder/controllers/partSettings.js
@@ -33,12 +33,6 @@ define( [], function( SettingGroupCollection ) {
 							label: 'Part Title',
 							width: 'full',
 						},
-						 {
-						 	name: 'mp_duplicate',
-						 	type: 'html',
-						 	width: 'one-half',
-						 	// value: '<a href="#" class="nf-duplicate-part nf-button secondary extra">Duplicate Part</a>'
-						 },
 						{
 							name: 'mp_remove',
 							type: 'html',
@@ -48,6 +42,21 @@ define( [], function( SettingGroupCollection ) {
 					]
 				},
 			] );
+			// only allow part duplication if Layouts & Styles exist
+			var formContentLoadFilters = nfRadio.channel( 'formContent'  ).request( 'get:loadFilters' );
+			if( 'undefined' != typeof formContentLoadFilters[4] ) {
+				console.log( this.collection.models );
+				 var colSettings  = this.collection.models[0].get( 'settings' );
+				 console.log(colSettings);
+					 colSettings.push(
+					{
+						name: 'mp_duplicate',
+						type: 'html',
+						width: 'one-half',
+						value: '<a href="#" class="nf-duplicate-part nf-button secondary extra">Duplicate Part</a>'
+					}
+				)
+			}
 		},
 
 		/**
